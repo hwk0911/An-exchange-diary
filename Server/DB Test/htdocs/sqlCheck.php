9f21eeab6915b7349e540c6c00186abf30627e88
hwk0911 <hwk0911@gmail.com>
2019-09-19 23:36:12 +0900

    MOD : PHP7 + Apache24 + mysql 연동에러 수정

--- Server/DB Test/htdocs/sqlCheck.php
+++ Server/DB Test/htdocs/sqlCheck.php
@@ -1,29 +1,23 @@
 <?php
 
-$mysql_hostname = 'localhost';
-$mysql_username = 'root';
-$mysql_password = 'root';
-$mysql_database = 'tutorials';
-$mysql_port = '3306';
+$mysqli_hostname = 'localhost';
+$mysqli_username = 'root';
+$mysqli_password = 'root';
+$mysqli_database = 'account';
+$mysqli_port = '3306';
 
-$connect = mysqli_connect($mysql_hostname, $mysql_username, $mysql_password, $mysql_database);
+$connect = mysqli_connect($mysqli_hostname, $mysqli_username, $mysqli_password, $mysqli_database);
 
-mysqli_select_db($connect, $mysql_database) or die('DB 선택 실패');
+mysqli_select_db($connect, $mysqli_database) or die('DB 선택 실패');
 
-$searchName = $_POST['name'];
-
-$sql = "SELECT * FROM tutorials WHERE searrch='$searchName' LIMIT 5";
+$sql = "SELECT * FROM account";
 
 $rs = mysqli_query($connect, $sql);
 
 while($info=mysqli_fetch_array($rs)){
 
-echo "Col1 : ".$info['필드명1']."<br/>";
-echo "Col2 : ".$info['필드명2']."<br/>";
-echo "Col3 : ".$info['필드명3']."<br/>";
-echo "Col4 : ".$info['필드명4']."<br/>";
-echo "Col5 : ".$info['필드명5']."<br/>";
-echo "Col6 : ".$info['필드명6']."<br/>"."<br/>";
+echo "ID : ".$info['ID']."<br/>";
+echo "PW : ".$info['PW']."<br/><br/>";
 
 }
 
