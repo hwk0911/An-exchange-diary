1d246a97ebfde5ee3a8fbed4c80b50d2c80afc30
dofany <kimdohwan17@gamil.com>
2019-09-23 22:40:09 +0900

    MOD: 데이터베이스 테이블 변경 및 생성

---
+++ Server/DB Query/20190923.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: an_exchange_diary
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `Id` varchar(200) NOT NULL DEFAULT '내 아이디',
+  `Pw` varchar(200) NOT NULL DEFAULT '내 비밀번호',
+  `Name` varchar(200) NOT NULL DEFAULT '내 이름',
+  `Email` varchar(200) NOT NULL DEFAULT '내 이메일',
+  `Age` varchar(200) NOT NULL DEFAULT '내 나이',
+  `Sex` varchar(200) NOT NULL DEFAULT '내 성별',
+  `Telnum` varchar(200) NOT NULL DEFAULT '내 휴대폰번호',
+  `User_Id` varchar(200) NOT NULL DEFAULT '상대방 아이디',
+  PRIMARY KEY (`Id`,`User_Id`),
+  KEY `User_Id` (`User_Id`),
+  CONSTRAINT `Id` FOREIGN KEY (`Id`) REFERENCES `adress` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `User_Id` FOREIGN KEY (`User_Id`) REFERENCES `adress` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `adress`
+--
+
+DROP TABLE IF EXISTS `adress`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `adress` (
+  `Id` varchar(200) NOT NULL DEFAULT '모든 아이디',
+  `Ip` varchar(200) NOT NULL DEFAULT '아이피',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adress`
+--
+
+LOCK TABLES `adress` WRITE;
+/*!40000 ALTER TABLE `adress` DISABLE KEYS */;
+INSERT INTO `adress` VALUES ('','0143DB63EE66B0CDFF9F69917680151E');
+/*!40000 ALTER TABLE `adress` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-23 22:36:51
